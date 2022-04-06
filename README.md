41d8a73fe4bd3837a2d3f163f555512a88886fe9
Borja Hernandez Crespo <bcrespo@confluent.io>
2022-04-06 17:22:06 +0200

    1st version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# training-developer-src-borja
+# Confluent Apache Kafka for Developers Course
+
+This is the source code accompanying the **Confluent Apache Kafka for Developers** course.
+
+It is organized in two subfolders `solution` and `labs`. The former contains the complete sample solution for each exercise whilst the latter contains the scaffoliding for each exercise and is meant to be used and elaborated on by the students during the hands-on.
+
+
---
+++ dotnet-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Installing .NET.....
+
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt update
+sudo apt install -y apt-transport-https
+sudo apt-get install -y dotnet-sdk-5.0
+sudo apt-get install -y aspnetcore-runtime-5.0
+sudo apt-get install -y dotnet-sdk-2.1
+sudo apt install -y aspnetcore-runtime-2.1
+
